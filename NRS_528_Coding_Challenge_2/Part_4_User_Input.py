df026b0b46627ff8478a9df76080857dc514b108
KelseyTichenor <kelsey.tichenor@uri.edu>
2024-02-08 20:02:18 -0500

    Finished Part 4, Starting Part 5

--- NRS_528_Coding_Challenge_2/Part_4_User_Input.py
+++ NRS_528_Coding_Challenge_2/Part_4_User_Input.py
@@ -5,3 +5,13 @@
 # Here's a hint:
 # age = input("What is your age?")
 # print("Your age is" + str(age))
+
+age = int(input("What is your age?"))
+retirement_age = 65
+years_until_retirement = (retirement_age - age)
+print("Congrats! We get to plan your retirement party in " + str(years_until_retirement) + " years!")
+
+#######
+
+# I'm guessing putting 'int' in front of an input function
+# tells the computer to expect an integer.
---
+++ NRS_528_Coding_Challenge_2/Part_5_User_Input_2.py
@@ -0,0 +1,16 @@
+# Coding Challenge 2: Part 5
+
+# Using the following dictionary (or a similar one you found on the internet),
+# ask the user for a word,
+# and compute the Scrabble word score for that word
+# steal this code from the internet, format it and make it work:
+# letter_scores = {
+#     "aeioulnrst": 1,
+#     "dg": 2,
+#     "bcmp": 3,
+#     "fhvwy": 4,
+#     "k": 5,
+#     "jx": 8,
+#     "qz": 10
+# }
+
