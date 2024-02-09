da3d26403f3605c63bcd4a65596a435201d9c29c
Kelsey Tichenor <kelsey.tichenor@uri.edu>
2024-02-09 15:06:30 -0500

    Adding Class Notes

---
+++ Class_3/Step_0.py
@@ -0,0 +1,23 @@
+
+#####
+# Step 0 - Practice tasks before we start.
+#####
+
+# Express these items as a list using list append (i.e. .append) and print it:
+
+item1 = "data1.txt"
+item2 = "data2.txt"
+item3 = "data3.txt"
+
+# Print out how many files are in the list? Hint, we can use len(NAME OF YOUR LIST)
+
+Step_0_List = [item1, item2]
+Step_0_List.append(item3)
+print(Step_0_List)
+len(Step_0_List)
+print("There are " + str(len(Step_0_List)) + " files in this list.")
+
+# Take this list of files (file_list), and using a for loop, go through each file name and add
+# a new file extension (.csv) and print new_extension_file_list.
+
+
