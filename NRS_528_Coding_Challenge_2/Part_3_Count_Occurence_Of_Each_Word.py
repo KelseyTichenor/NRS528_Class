4b6592162c3a9f835c16839fded6c6e3e7fa22b9
Kelsey Tichenor <157842387+KelseyTichenor@users.noreply.github.com>
2024-02-08 19:14:24 -0500

    Delete NRS_528_Coding_Challenge_2/Part_3_Count_Occurence_Of_Each_Word.py

--- NRS_528_Coding_Challenge_2/Part_3_Count_Occurence_Of_Each_Word.py
+++
@@ -1,9 +0,0 @@
-# Coding Challenge 2: Part 3
-
-# Given a single phrase, count the occurrence of each word:
-# We'll be using the following string:
-#   string = 'hi dee hi how are you mr dee'
-# Count the occurrence of each word, and print the word plus the count.
-# Here's a hint: you might want to "split" this into a list by a white space: " ".
-
-words_in_string = ['hi', 'dee', 'hi', 'how', 'are', 'you', 'mr', 'dee']
