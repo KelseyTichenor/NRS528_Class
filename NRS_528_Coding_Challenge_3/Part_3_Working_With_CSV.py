88fbe4303d74f19845fa714961dd654cc8d27107
KelseyTichenor <kelsey.tichenor@uri.edu>
2024-02-16 11:14:34 -0500

    Uploaded CC3 Files So Far

---
+++ NRS_528_Coding_Challenge_3/Part_3_Working_With_CSV.py
@@ -0,0 +1,15 @@
+# Coding Challenge 3: Part 3
+
+# Using the Atmospheric Carbon Dioxide Dry Air Mole Fractions
+# from quasi-continuous daily measurements at Mauna Loa, Hawaii dataset,
+# Use Python (csv) calculate the following:
+#
+# 1. Annual average for each year in the dataset.
+# 2. Minimum, maximum and average for the entire dataset.
+# 3. Seasonal average of:
+#       Spring (March, April, May),
+#       Summer (June, July, August),
+#       Autumn (September, October, November)
+#       and Winter (December, January, February).
+# 4. Calculate the anomaly for each value in the dataset
+#       relative to the mean for the entire time series.
