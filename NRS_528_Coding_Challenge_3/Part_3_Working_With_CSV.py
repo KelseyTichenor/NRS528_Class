78c883b2b057d7d18f003362bb640947f21b6fdf
KelseyTichenor <kelsey.tichenor@uri.edu>
2024-02-16 11:17:17 -0500

    Update Part_3_Working_With_CSV.py

--- NRS_528_Coding_Challenge_3/Part_3_Working_With_CSV.py
+++ NRS_528_Coding_Challenge_3/Part_3_Working_With_CSV.py
@@ -13,3 +13,23 @@
 #       and Winter (December, January, February).
 # 4. Calculate the anomaly for each value in the dataset
 #       relative to the mean for the entire time series.
+
+import pandas
+
+years = []
+co2_Values = []
+
+with open("co2-ppm-daily.csv") as co2_csv:
+    next(co2_csv) #skip first line
+    for row in co2_csv:
+        years.append(row[3])
+        co2_Values.append(row[1])
+        print(row)
+    #     total += float(row[3]) #3 = 4th column, in this case, population, remember Python uses zero indexing
+    # print(format(total, 'f')) # format prints as float
+    # print(total) # without we print as engineering notation
+print(years)
+print(co2_Values)
+
+# Okay, why is Pycharm treating each number in the dataset like it's its own row?
+# For the year 1958: Row 0 = 1, Row 1 = 9, Row 2 = 5, Row 3 = 8
