0757947fed18963404e18fc5ae12931164d98b13
KelseyTichenor <kelsey.tichenor@uri.edu>
2024-02-15 19:53:35 -0500

    Streamlined the code in Challenge 3 a bit

--- NRS_528_Coding_Challenge_3/Part_1_Simple_Directory_Tree.py
+++ NRS_528_Coding_Challenge_3/Part_1_Simple_Directory_Tree.py
@@ -18,45 +18,52 @@
 
 import os
 
-# directory = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree'
-# os.mkdir(directory)
-#
-# filepath_1 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\draft_code'
-# os.mkdir(filepath_1)
-#
-# subdirectory_1A = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\draft_code\pending'
-# os.mkdir(subdirectory_1A)
-#
-# subdirectory_1B = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\draft_code\complete'
-# os.mkdir(subdirectory_1B)
-#
-# filepath_2 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\includes'
-# os.mkdir(filepath_2)
-#
-# filepath_3 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts'
-# os.mkdir(filepath_3)
-#
-# subdirectory_2A = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts\default'
-# os.mkdir(subdirectory_2A)
-#
-# subdirectory_2B = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts\post'
-# os.mkdir(subdirectory_2B)
-#
-# subsubdirectory = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts\post\posted'
-# os.mkdir(subsubdirectory)
-#
-# filepath_4 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\site'
-# os.mkdir(filepath_4)
-#
-# list = os.listdir(directory)
-# print(list)
-# for folder_name in list:
-#     if folder_name == "draft_code" or folder_name == "includes" or folder_name == "layouts" or folder_name == "site":
-#       print("Yay, making this directory worked!")
-#     elif folder_name == "pending" or folder_name == "complete" or folder_name == "default" or folder_name == "post" or folder_name == "posted":
-#       print("Cool, just making sure the subdirectories work too.")
-#     else:
-#       print("Check your code, you screwed up somewhere.")
+directory = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree'
+os.mkdir(directory)
+
+filepath_1 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\draft_code'
+os.mkdir(filepath_1)
+
+subdirectory_1A = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\draft_code\pending'
+os.mkdir(subdirectory_1A)
+
+subdirectory_1B = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\draft_code\complete'
+os.mkdir(subdirectory_1B)
+
+filepath_2 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\includes'
+os.mkdir(filepath_2)
+
+filepath_3 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts'
+os.mkdir(filepath_3)
+
+subdirectory_2A = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts\default'
+os.mkdir(subdirectory_2A)
+
+subdirectory_2B = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts\post'
+os.mkdir(subdirectory_2B)
+
+subsubdirectory = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts\post\posted'
+os.mkdir(subsubdirectory)
+
+filepath_4 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\site'
+os.mkdir(filepath_4)
+
+list = os.listdir(directory)
+print(list)
+for folder_name in list:
+    if (folder_name == "draft_code" or
+            folder_name == "includes" or
+            folder_name == "layouts" or
+            folder_name == "site"):
+      print("Yay, making this directory worked!")
+    elif (folder_name == "pending" or
+          folder_name == "complete" or
+          folder_name == "default" or
+          folder_name == "post" or
+          folder_name == "posted"):
+      print("Cool, just making sure the subdirectories work too.")
+    else:
+      print("Check your code, you screwed up somewhere.")
 
 ######
 
@@ -81,41 +88,41 @@ import os
 
 #######
 
-subsubdirectory = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts\post\posted'
-
-subdirectory_1A = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\draft_code\pending'
-subdirectory_1B = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\draft_code\complete'
-subdirectory_2A = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts\default'
-subdirectory_2B = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts\post'
-
-filepath_1 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\draft_code'
-filepath_2 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\includes'
-filepath_3 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts'
-filepath_4 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\site'
-
-directory = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree'
-
-os.rmdir(subsubdirectory)
-
-os.rmdir(subdirectory_1A)
-os.rmdir(subdirectory_1B)
-
-os.rmdir(subdirectory_2A)
-os.rmdir(subdirectory_2B)
-
-os.rmdir(filepath_1)
-os.rmdir(filepath_2)
-os.rmdir(filepath_3)
-os.rmdir(filepath_4)
-
-os.rmdir(directory)
-
-directory_parent_folder = r'C:\Users\ektic'
-
-os.listdir(directory_parent_folder)
-
-for folder in os.listdir(directory_parent_folder):
-    if folder == r'Coding_Challenge_3_Directory_Tree':
-        print("Oops, try again")
-    else:
-        print("All gone!")
+# subsubdirectory = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts\post\posted'
+#
+# subdirectory_1A = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\draft_code\pending'
+# subdirectory_1B = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\draft_code\complete'
+# subdirectory_2A = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts\default'
+# subdirectory_2B = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts\post'
+#
+# filepath_1 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\draft_code'
+# filepath_2 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\includes'
+# filepath_3 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\layouts'
+# filepath_4 = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree\site'
+#
+# directory = r'C:\Users\ektic\Coding_Challenge_3_Directory_Tree'
+#
+# os.rmdir(subsubdirectory)
+#
+# os.rmdir(subdirectory_1A)
+# os.rmdir(subdirectory_1B)
+#
+# os.rmdir(subdirectory_2A)
+# os.rmdir(subdirectory_2B)
+#
+# os.rmdir(filepath_1)
+# os.rmdir(filepath_2)
+# os.rmdir(filepath_3)
+# os.rmdir(filepath_4)
+#
+# os.rmdir(directory)
+#
+# directory_parent_folder = r'C:\Users\ektic'
+#
+# os.listdir(directory_parent_folder)
+#
+# for folder in os.listdir(directory_parent_folder):
+#     if folder == r'Coding_Challenge_3_Directory_Tree':
+#         print("Oops, try again")
+#     else:
+#         print("All gone!")
---
+++ NRS_528_Coding_Challenge_3/Part_2_Push_SysArgv_To_The_Limit.py
@@ -0,0 +1,10 @@
+# Coding Challenge 3: Part 2
+
+# Construct a rudimentary Python script that takes a series of inputs from a bat file,
+#   and does something to them.
+# The rules:
+#   1. Minimum of three arguments to be used.
+#   2. You must do something simple in 15 lines or less within the Python file.
+#   3. Print or file generated output should be produced.
+
+# The 15 lines will start on Line 11. Ready, Set, GO!
