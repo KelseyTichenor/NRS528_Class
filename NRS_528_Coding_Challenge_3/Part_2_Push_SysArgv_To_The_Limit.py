ca043519d0b973d68592b86cedc2a5764f461888
KelseyTichenor <kelsey.tichenor@uri.edu>
2024-02-16 11:07:24 -0500

    Trying to fix files a bit before emailing

--- NRS_528_Coding_Challenge_3/Part_2_Push_SysArgv_To_The_Limit.py
+++ NRS_528_Coding_Challenge_3/Part_2_Push_SysArgv_To_The_Limit.py
@@ -7,4 +7,17 @@
 #   2. You must do something simple in 15 lines or less within the Python file.
 #   3. Print or file generated output should be produced.
 
-# The 15 lines will start on Line 11. Ready, Set, GO!
+# When creating a batch file, start out with the python interpreter filepath.
+# Then afterwards, add the Python file you want the batch file to interface with (?)
+# Then list your arguments on one line (?)
+# Do all three steps on one line. On the next line, type 'pause'
+#   so it doesn't immediately close out.
+
+#######
+
+import sys
+
+print("Pizza Topping 1 = " + str(sys.argv[1]))
+print("Pizza Topping 2 = " + str(sys.argv[2]))
+print("Pizza Topping 3 = " + str(sys.argv[3]))
+
