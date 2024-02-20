a670d8ddf21714265e469b8eaf0adb849c3d5da7
KelseyTichenor <kelsey.tichenor@uri.edu>
2024-02-20 18:05:56 -0500

    Starting the challenge

---
+++ NRS_528_Coding_Challenge_4/RI_Wetlands_2020_Tool.py
@@ -0,0 +1,8 @@
+# Coding Challenge 4: Let's make a tool!
+
+# Requirements:
+# Code must be well-commented.
+# Code must be able to run on any machine with a simple filepath change.
+
+# Step 1: Open up ArcPro and import arcpy.
+import arcpy
