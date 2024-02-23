30f6c2f4e7e1ddc3cfa98e111ced42f3f0bd811e
Kelsey Tichenor <kelsey.tichenor@uri.edu>
2024-02-23 13:33:18 -0500

    Will fix this later - time for class

--- NRS_528_Coding_Challenge_3/Part_3_Working_With_CSV.py
+++ NRS_528_Coding_Challenge_3/Part_3_Working_With_CSV.py
@@ -10,11 +10,28 @@ import csv
 
 years = []
 entire_co2_dataset = []
+Jan = 1
+Feb = 2
+Mar = 3
+Apr = 4
+May = 5
+Jun = 6
+Jul = 7
+Aug = 8
+Sep = 9
+Oct = 10
+Nov = 11
+Dec = 12
+spring = [3,4,5]
+summer = [6,7,8]
+autumn = [9,10,11]
+winter = [12,1,2]
 
 with open("co2-ppm-daily.csv") as co2csv:
     next(co2csv)
     for row in csv.reader(co2csv, delimiter=','):
         year = row[0].split("-")[0]
+        month = row[0].split("-")[1]
         entire_co2_dataset.append(float(row[1]))
         if year not in years:
             years.append(year)
@@ -40,7 +57,7 @@ print(str("The mean for this dataset is " + str(sum(entire_co2_dataset)/len(enti
 #       Summer (June, July, August) (summer = june + july + august),
 #       Autumn (September, October, November)
 #       and Winter (December, January, February).
-
+print(month)
 # 4. Calculate the anomaly for each value in the dataset (average minus value)
 #       relative to the mean for the entire time series.
 #
