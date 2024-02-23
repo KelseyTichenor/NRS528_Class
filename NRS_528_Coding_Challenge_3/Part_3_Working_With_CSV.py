3a63c6a9e29b266dbaf682af54b4a83462030bbe
Kelsey Tichenor <kelsey.tichenor@uri.edu>
2024-02-23 13:19:33 -0500

    Office Hours 2/23/24

--- NRS_528_Coding_Challenge_3/Part_3_Working_With_CSV.py
+++ NRS_528_Coding_Challenge_3/Part_3_Working_With_CSV.py
@@ -9,51 +9,41 @@
 import csv
 
 years = []
-co2values = []
+entire_co2_dataset = []
 
 with open("co2-ppm-daily.csv") as co2csv:
     next(co2csv)
-    for row in csv.reader(co2csv, delimiter='-'):
-        if row[0] not in years:
-            years.append(row[0])
+    for row in csv.reader(co2csv, delimiter=','):
+        year = row[0].split("-")[0]
+        entire_co2_dataset.append(float(row[1]))
+        if year not in years:
+            years.append(year)
 
-    for year in years:
-        with open("co2-ppm-daily.csv") as co2csv:
-            saved_header = next(co2csv)
-            file = open(r"years/" + year + ".txt", "w")
-            file.write(saved_header)
-            for row in csv.reader(co2csv):
-                if year == row[0]:
-                    file.write(", ".join(row))
-                    file.write("\n")
-            file.close()
+# 1. Annual average
+for year in years:
+    co2values = []
+    with open("co2-ppm-daily.csv") as co2csv:
+        next(co2csv)
+        for row in csv.reader(co2csv, delimiter=','):
+            row_year = row[0].split("-")[0]
+            if row_year == year:
+                co2values.append(float(row[1]))
+    print(str(year) + ": " + str(sum(co2values)/len(co2values)))
+#
+# # 2. Minimum, maximum and average for the entire dataset.
+print(str("The min for this dataset is " + str(min(entire_co2_dataset))))
+print(str("The max for this dataset is " + str(max(entire_co2_dataset))))
+print(str("The mean for this dataset is " + str(sum(entire_co2_dataset)/len(entire_co2_dataset))))
 
-with open("co2-ppm-daily.csv") as co2csv:
-    next(co2csv)
-    for row in csv.reader(co2csv):
-            co2values.append(row[1])
-
-# print(len(years))
-# print(years)
-# print(co2values)
-
-# 2. Minimum, maximum and average for the entire dataset.
-# 3. Seasonal average of:
-#       Spring (March, April, May),
-#       Summer (June, July, August),
-#       Autumn (September, October, November)
-#       and Winter (December, January, February).
-# 4. Calculate the anomaly for each value in the dataset
-#       relative to the mean for the entire time series.
-
-#######
-
-# Need 'import csv' in order to actually use the CSV properly
-# delimiter: I noticed that '-' let me isolate just the years,
-#       which is what I wanted for Question 1.
-# Not sure how to separate months from days from years in the 'date' row
-#       The delimiter seemed to do this, but it doesn't separate the data at all.
-# Creating two separate loops will create a list of years and list of CO2 values.
-# Starting on Line 20 and ending Line 29: code from Task 3 - Final Task Video
-#       Not recording years in each file, because date components not separated?
-#       Tried positioning this block after the block on Line 31: same result
+# # 3. Seasonal average of:
+# #       Spring (March, April, May),
+# #       Summer (June, July, August) (this month = june + july + august),
+# #       Autumn (September, October, November)
+# #       and Winter (December, January, February).
+# # 4. Calculate the anomaly for each value in the dataset (average minus value)
+# #       relative to the mean for the entire time series.
+#
+# #######
+# Notes:
+# Set up the dataset the right way before attemoting to answer any questions.
+#
