3bf4ad84f03dfcb8e2c0697d2fa4f13516811bd4
KelseyTichenor <kelsey.tichenor@uri.edu>
2024-02-23 23:14:34 -0500

    Finished Class 5 Task 3 (potentially CC5)

--- Class_5/Step_3.py
+++ Class_5/Step_3.py
@@ -13,7 +13,7 @@
 
 import arcpy
 
-arcpy.env.workspace = r"E:\Kelsey_Tichenor_NRS_528\GitHub\NRS528_Class\Class_5"
+arcpy.env.workspace = r"C:\GitHub\NRS528_Class\Class_5"
 arcpy.env.overwriteOutput = True
 
 in_Table = r"Step_3_Cepphus_grylle.csv"
@@ -23,7 +23,7 @@ y_coords = "lat"
 # At first, I transposed latitude and longitude - are they supposed to be backwards in this situation?
 z_coords = ""
 out_Layer = "Cepphus Grylle"
-saved_Layer = r"E:\Kelsey_Tichenor_NRS_528\GitHub\NRS528_Class\Class_5\Step_3_Cepphus_grylle.shp"
+saved_Layer = r"C:\GitHub\NRS528_Class\Class_5\Step_3_Cepphus_grylle.shp"
 
 # Set the spatial reference
 spRef = arcpy.SpatialReference(4326)  # 4326 == WGS 1984
@@ -37,38 +37,70 @@ if arcpy.Exists(saved_Layer):
     print("Created file successfully!")
 
 # 2. Extact the Extent, i.e. XMin, XMax, YMin, YMax of the generated Cepphus_grylle shapefile.
-desc = arcpy.Describe(r"E:\Kelsey_Tichenor_NRS_528\GitHub\NRS528_Class\Class_5\Step_3_Cepphus_grylle.shp")
+
+desc = arcpy.Describe(r"C:\GitHub\NRS528_Class\Class_5\Step_3_Cepphus_grylle.shp")
 # print(desc.extent) - but string substitution notation makes things more accessible, so let's do this instead:
-print("Extent:\n  XMin: {0},\n XMax: {1},\n YMin: {2},\n YMax: {3}".format(desc.extent.XMin, desc.extent.XMax, desc.extent.YMin, desc.extent.YMax))
+print("Extent:\n XMin: {0},\n XMax: {1},\n YMin: {2},\n YMax: {3}".format(desc.extent.XMin, desc.extent.XMax, desc.extent.YMin, desc.extent.YMax))
+# When we run this step:
+#  XMin: -82.0,
+#  XMax: -50.45,
+#  YMin: 36.79312,
+#  YMax: 59.4333
 
 # 3. Generate a fishnet, but this time define the originCoordinate, yAxisCoordinate and oppositeCorner
 # using the extracted extent from above. Hint: Formatting of the coordinate is important when generating
 # the Fishnet, you must present it as: "-176.87 -41", note the space inbetween, and the fact that the
 # entire thing is a string. Hint use: cellSizes of 0.25 degrees.
 
-print(desc.origin_coord)
+# Keep in mind: to find the origin point from the calculated extent:
+#   It's literally just (XMin, YMin). Doubts? Calculate the extent from Step_1 and compare with the origin in Step_2.
 
-# originCoordinate = "-176.87 -51"  # Left bottom of our point data
-# yAxisCoordinate = "-176.87 -41"  # This sets just the orientation of the y-axis, so we head 10 degrees perfectly north
-# cellSizeWidth = "10"  # 10 degrees - these cells are going to be big to start with, we can refine later.
-# cellSizeHeight = "10"
-# numRows = ""  # Leave blank, as we have set cellSize
-# numColumns = ""  # Leave blank, as we have set cellSize
-# oppositeCorner = "162.0200043 71.34999847"  # starts in the bottom left corner, should end in top right corner
-# # On your own time, make this as weird as possible just so you get comfortable with this tool.
-# labels = "NO_LABELS" # don't want to deal with them here
-# templateExtent = "#"  # No need to use, as we have set yAxisCoordinate and oppositeCorner
-# geometryType = "POLYGON"  # Create a polygon, so we can count the points inside the polygon.
-# # could be POLYLINE, but can't do spatial analysis on the inside.
+outFeatureClass = "Step_3_Fishnet.shp"  # Name of output fishnet
+# Set the origin of the fishnet - remember, the coordinates will ALWAYS be in degrees relative to the equator.
+originCoordinate = "-82.0 36.79312"  # Left bottom of our point data
+yAxisCoordinate = "-82.0 46.79312"  # This sets just the orientation of the y-axis, so we head 10 degrees perfectly north
+cellSizeWidth = "0.25"  # if not specified, start with 10 degrees; we can refine later.
+cellSizeHeight = "0.25"
+numRows = ""  # Leave blank, as we have set cellSize
+numColumns = ""  # Leave blank, as we have set cellSize
+oppositeCorner = "-50.45 59.4333"  # starts in the bottom left corner, should end in top right corner
+# On your own time, do try to make this as weird as possible so you can see how the program works.
+# Again, this depends on the extent: should be (XMax, YMax)
+labels = "NO_LABELS" # don't want to deal with them here
+templateExtent = "#"  # No need to use, as we have set yAxisCoordinate and oppositeCorner
+geometryType = "POLYGON"  # Create a polygon, so we can count the points inside the polygon.
+# could be POLYLINE, but can't do spatial analysis on the inside.
+
+arcpy.CreateFishnet_management(outFeatureClass, originCoordinate, yAxisCoordinate,
+                               cellSizeWidth, cellSizeHeight, numRows, numColumns,
+                               oppositeCorner, labels, templateExtent, geometryType)
+
+# WARNING: for some reason what should be the origin corner is acting as the opposite corner.
+# UPDATE: this is because before, I set the y-axis coordinate to (-82.0 26.79312) instead.
+#         This will force the fishnet points to be reflected below where we need them to be.
 #
-# arcpy.CreateFishnet_management(outFeatureClass, originCoordinate, yAxisCoordinate,
-#                                cellSizeWidth, cellSizeHeight, numRows, numColumns,
-#                                oppositeCorner, labels, templateExtent, geometryType)
-
 # 4. Undertake a Spatial Join to join the fishnet to the observed points.
+#
+# Recall from Step 2 Part B:
+# "To (count the amount of points in each fishnet cell),
+# we use a tool called SpatialJoin, relatively simple to set up"
+
+target_features="Step_3_Fishnet.shp"
+join_features="Step_3_Cepphus_grylle.shp"
+out_feature_class="Step_3_Cepphus-grylle_HeatMap.shp"
+join_operation="JOIN_ONE_TO_ONE"
+join_type="KEEP_ALL" # keep all the polygon shapefiles even if no specimens are in it
+field_mapping=""
+match_option="INTERSECT"
+search_radius=""
+distance_field_name=""
+
+arcpy.SpatialJoin_analysis(target_features, join_features, out_feature_class,
+                           join_operation, join_type, field_mapping, match_option,
+                           search_radius, distance_field_name)
 
 # 5. Check that the heatmap is created and delete the intermediate files (e.g. species shapefile and fishnet). Hint:
-# arcpy.Delete_management()..
+arcpy.Delete_management("Step_3_Cepphus_grylle.shp", "Step_3_Fishnet.shp")
 
 # 6. Visualize in ArcGIS Pro
 
--- NRS_528_Coding_Challenge_3/Part_3_Working_With_CSV.py
+++ NRS_528_Coding_Challenge_3/Part_3_Working_With_CSV.py
@@ -10,50 +10,54 @@ import csv
 
 years = []
 entire_co2_dataset = []
-months = []
+springCo2 = []
+summerCo2 = []
+autumnCo2 = []
+winterCo2 = []
 
 with open("co2-ppm-daily.csv") as co2csv:
     next(co2csv)
     for row in csv.reader(co2csv, delimiter=','):
         year = row[0].split("-")[0]
-        month = row[0].split("-")[1]
         entire_co2_dataset.append(float(row[1]))
         if year not in years:
             years.append(year)
-        if month not in months:
-            months.append(month)
-        for month in months:
-            spring = [str('03'), str('04'), str('05')]
-            summer = [str('06'), str('07'), str('08')]
-            autumn = [str('09'), str('10'), str('11')]
-            winter = [str('12'), str('01'), str('02')]
 
-# 1. Annual average
-for year in years:
-    co2values = []
-    with open("co2-ppm-daily.csv") as co2csv:
-        next(co2csv)
-        for row in csv.reader(co2csv, delimiter=','):
-            row_year = row[0].split("-")[0]
-            if row_year == year:
-                co2values.append(float(row[1]))
-    print(str(year) + ": " + str(sum(co2values)/len(co2values)))
-
-# 2. Minimum, maximum and average for the entire dataset.
-print(str("The min for this dataset is " + str(min(entire_co2_dataset))))
-print(str("The max for this dataset is " + str(max(entire_co2_dataset))))
-print(str("The mean for this dataset is " + str(sum(entire_co2_dataset)/len(entire_co2_dataset))))
+with open("co2-ppm-daily.csv") as co2csv:
+    next(co2csv)
+    for row in csv.reader(co2csv, delimiter=','):
+        month = row[0].split("-")[1]
+        if month == "03" or "04" or "05":
+             springCo2.append(float(row[1]))
+    print(springCo2)
+# # 1. Annual average
+# for year in years:
+#     co2values = []
+#     with open("co2-ppm-daily.csv") as co2csv:
+#         next(co2csv)
+#         for row in csv.reader(co2csv, delimiter=','):
+#             row_year = row[0].split("-")[0]
+#             if row_year == year:
+#                 co2values.append(float(row[1]))
+#     print(str(year) + ": " + str(sum(co2values)/len(co2values)))
+#
+# # 2. Minimum, maximum and average for the entire dataset.
+# print(str("The min for this dataset is " + str(min(entire_co2_dataset))))
+# print(str("The max for this dataset is " + str(max(entire_co2_dataset))))
+# print(str("The mean for this dataset is " + str(sum(entire_co2_dataset)/len(entire_co2_dataset))))
 
 # 3. Seasonal average of:
 #       Spring (March, April, May),
 #       Summer (June, July, August) (summer = june + july + august),
 #       Autumn (September, October, November)
 #       and Winter (December, January, February).
+
+
 # 4. Calculate the anomaly for each value in the dataset (average minus value)
 #       relative to the mean for the entire time series.
 #
 # #######
 # Notes:
-# Set up the dataset the right way before attemoting to answer any questions.
+# Set up the dataset the right way before attempting to answer any questions.
 # It's important to know how the CSV works before trying to code using it
 #
